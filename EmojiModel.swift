fd96a92b2a7307e020f60a5aba743881e13d21f4
MaximilianMalsagov <117158527+MaximilianMalsagov@users.noreply.github.com>
2022-12-03 15:19:52 +0300

    Add files via upload

---
+++ EmojiModel.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Maximilian on 02.12.2022.
+//
+
+import Foundation
+
+
+struct Emoji {
+    
+    var emoji : String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+    
+}
